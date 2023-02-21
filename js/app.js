7c181af12d1f56539d8d22d11478602cbba540fd
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-21 14:59:40 +0100

    FEAT arrayobject in js + add text in html+style

--- js/app.js
+++ js/app.js
@@ -3,7 +3,21 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-        
+            tasks : [
+                {
+                    text: 'Rispondere alle email',
+                    done : true
+                },
+                {
+                    text: 'Lavorare al progetto',
+                    done : false
+                },
+                {
+                    text: 'Ripassare nuovi argomenti',
+                    done : false
+                }
+            ],
         }
     }
 }).mount('#app')
+
